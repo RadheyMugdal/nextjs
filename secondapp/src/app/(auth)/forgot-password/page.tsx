4bc50ce34f28e27a1e651285d4da7cf58bd2f1f1
Radhey Mugdal <mugdalradhey@gmail.com>
2024-03-09 22:14:23 +0530

    learned about layout and group routing

---
+++ secondapp/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      Forgot Password
+    </div>
+  )
+}
+
+export default page
---
+++ secondapp/src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      Register
+    </div>
+  )
+}
+
+export default page
